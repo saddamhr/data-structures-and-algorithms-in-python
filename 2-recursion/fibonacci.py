89831bbcd836f05ab417369a8b8762dec470b855
Md_Saddam_Hossain <saddam.hossain@selise.ch>
2023-02-25 11:44:28 +0600

    End of Recursion

---
+++ 2-recursion/fibonacci.py
@@ -0,0 +1,8 @@
+def fibonacci(n):
+    assert n >=0 and int(n) == n, 'Fibonacci number cannot be negative number of non integer.'
+    if n in [0, 1]:
+        return n
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+
+print(fibonacci(4))
